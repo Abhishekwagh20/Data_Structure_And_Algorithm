82ddb468ac8b784252e9da240b8661db9d9478d7
Abhishek Wagh <abhimwagh20@gmail.com>
2023-01-22 17:10:15 -0600

    Create PrimeFactorization.java

---
+++ PrimeFactorization.java
@@ -0,0 +1,16 @@
+package Data_Structure_And_Algorithm;
+
+import java.util.Scanner;
+
+public class PrimeFactorization {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for(int div = 2; div < num; div++){
+
+        }
+
+    }
+}
