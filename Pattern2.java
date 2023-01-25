95e37b5adc880bb7665e4dbab963b2562075eb88
Abhishek Wagh <abhimwagh20@gmail.com>
2023-01-25 13:25:35 -0600

    Create Pattern2.java

---
+++ Pattern2.java
@@ -0,0 +1,25 @@
+package Data_Structure_And_Algorithm;
+//        1. You are given a number n.
+//        2. You've to create a pattern of * and separated by tab as shown in output format.
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//5
+//        *	*	*	*	*
+//        *	*	*	*
+//        *	*	*
+//        *	*
+//        *
