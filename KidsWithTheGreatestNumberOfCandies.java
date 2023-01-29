1e1811a8992045a90bb63267a9f81bcebe8ea1d3
Abhishek Wagh <abhimwagh20@gmail.com>
2023-01-29 17:29:49 -0600

    Create KidsWithTheGreatestNumberOfCandies.java

---
+++ KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,22 @@
+package Data_Structure_And_Algorithm;
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+public class KidsWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> result = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for(int i: candies){
+            if(i> max){
+                max = i;
+            }
+        }
+
+        for(int i: candies){
+            if(i+extraCandies >= max){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
