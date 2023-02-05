029e18f733bbc3061b892a0c0957224934fa98e9
Abhishek Wagh <abhimwagh20@gmail.com>
2023-02-04 21:55:32 -0600

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,32 @@
+package Data_Structure_And_Algorithm;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int sp = n/2;
+        int st = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+            }
+
+            for(int k=1;k<=st;k++){
+                System.out.print("*\t");
+            }
+
+            if(i<=n/2){
+                sp--;
+                st += 2;
+            }else{
+                sp++;
+                st -= 2;
+            }
+            System.out.println();
+        }
+    }
+}
