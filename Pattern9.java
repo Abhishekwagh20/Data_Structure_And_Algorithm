fd1bd466e5e53dae4f1e5ff98043b94892b5e94e
Abhishek Wagh <abhimwagh20@gmail.com>
2023-02-05 12:37:08 -0600

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,21 @@
+package Data_Structure_And_Algorithm;
+
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j=1; j<=n ;j++){
+                if(i+j == n+1 || i == j){
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
