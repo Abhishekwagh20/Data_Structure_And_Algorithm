f258f98adf1f3df1c6519beb964ccba5f9c04927
Abhishek Wagh <abhimwagh20@gmail.com>
2023-02-04 22:04:32 -0600

    Create Pattern6.java

---
+++ Pattern6.java
@@ -0,0 +1,37 @@
+package Data_Structure_And_Algorithm;
+
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int st = n/2 + 1;
+        int sp = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
+            }
+
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
+            }
+
+            if(i <= n/2){
+                st--;
+                sp += 2;
+            }else{
+                st++;
+                sp -= 2;
+            }
+            System.out.println();
+        }
+    }
+}
