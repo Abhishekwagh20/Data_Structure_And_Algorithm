4741bf775e1a81520b7c80ffc51e60ed6b0c2843
Abhishek Wagh <abhimwagh20@gmail.com>
2023-02-05 12:53:34 -0600

    Create Pattern10.java

---
+++ Pattern10.java
@@ -0,0 +1,37 @@
+package Data_Structure_And_Algorithm;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int os = n/2;
+        int is = -1;
+
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <=os ; j++) {
+                System.out.print("\t");
+            }
+            System.out.print("*\t");
+
+            for (int j = 1; j <= is; j++) {
+                System.out.print("\t");
+            }
+
+            if(i>1 && i<n){
+                System.out.print("*\t");
+            }
+
+            if(i <= n/2){
+                os--;
+                is += 2;
+            }else{
+                os++;
+                is -=2;
+            }
+            System.out.println();
+        }
+    }
+}
