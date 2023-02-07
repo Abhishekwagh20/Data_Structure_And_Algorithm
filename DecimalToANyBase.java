c25dde56dfa8d20e9b9c944f7398e57ec1b34381
Abhishek Wagh <abhimwagh20@gmail.com>
2023-02-07 14:06:34 -0600

    Create DecimalToANyBase.java

---
+++ DecimalToANyBase.java
@@ -0,0 +1,24 @@
+package Data_Structure_And_Algorithm;
+
+import java.util.Scanner;
+
+public class DecimalToANyBase {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(convert(n, b));
+    }
+
+    public static int convert(int n, int b){
+        int sum = 0;
+        int p = 1;
+        while (n > 0){
+            int r = n % b;
+            sum += r * p;
+            p *= 10;
+            n /= b;
+        }
+        return sum;
+    }
+}
