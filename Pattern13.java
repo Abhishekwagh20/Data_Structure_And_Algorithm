7cd7fe081255c84e2c4ae9a4a8d8fd5debd2b10d
Abhishek Wagh <62249806+Abhishekwagh20@users.noreply.github.com>
2023-06-24 18:54:20 -0500

    Update Pattern13.java

--- Pattern13.java
+++ Pattern13.java
@@ -1,4 +1,19 @@
 package Data_Structure_And_Algorithm;
+import java.util.Scanner;
 
 public class Pattern13 {
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i = 0 ;i < n; i++){
+            int icj = 1;
+            for(int j = 0; j <= i ; j++){
+                System.out.print(icj +"\t");
+                int icj1 = icj * (i-j) / (j+1);
+                icj = icj1;
+            }
+            System.out.println("\t");
+        }
+    }
 }
