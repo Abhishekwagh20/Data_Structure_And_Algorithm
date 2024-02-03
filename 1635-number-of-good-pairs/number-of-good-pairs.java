a860080eee610fe03a8c86c9bb86fbc661f80806
Abhishek Wagh <62249806+Abhishekwagh20@users.noreply.github.com>
2024-02-03 15:13:32 -0600

    Time: 0 ms (100.00%) | Memory: 40.5 MB (79.81%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for(int i = 0; i < nums.length - 1; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-        }
-        return count;
+       int ct = 0;
+       int[] temp = new int[101];
+
+       for(int i : nums){
+           ct += temp[i];
+           temp[i]++;
+       }
+
+       return ct;
     }
 }
