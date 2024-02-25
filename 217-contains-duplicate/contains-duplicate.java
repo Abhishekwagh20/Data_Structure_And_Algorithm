0727f629e246dfa1df70693b0b865919e219194e
Abhishek Wagh <62249806+Abhishekwagh20@users.noreply.github.com>
2024-02-25 16:39:37 -0600

    Time: 17 ms (40.84%) | Memory: 58.6 MB (48.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -7,5 +7,14 @@ class Solution {
         }
 
         return set.size() != nums.length;
+
+        // for(int i = 0; i < nums.length - 1; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
     }
 }
