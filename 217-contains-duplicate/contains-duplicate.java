b75ef8179666912ef0f73291413cd553aeefb04f
Abhishek Wagh <62249806+Abhishekwagh20@users.noreply.github.com>
2024-03-04 12:18:25 -0600

    Time: 15 ms (38.51%) | Memory: 58.3 MB (56.09%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,10 +3,13 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for(int n : nums){
+            if(set.contains(n)){
+                return true;
+            }
             set.add(n);
         }
 
-        return set.size() != nums.length;
+        return false;
 
         // for(int i = 0; i < nums.length - 1; i++){
         //     for(int j = i+1; j < nums.length; j++){
