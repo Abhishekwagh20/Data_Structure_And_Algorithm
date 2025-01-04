2ab2314de0125bef55546ccc9c1c58692b1283ee
Abhishek Wagh <62249806+Abhishekwagh20@users.noreply.github.com>
2025-01-04 13:43:06 -0600

    Time: 3 ms (72.87%) | Memory: 42.9 MB (84.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,32 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder ans = new StringBuilder();
-        for(char c : s.toCharArray()){
-            if(Character.isLetterOrDigit(c)){
-                ans.append(Character.toLowerCase(c));
+        // StringBuilder ans = new StringBuilder();
+        // for(char c : s.toCharArray()){
+        //     if(Character.isLetterOrDigit(c)){
+        //         ans.append(Character.toLowerCase(c));
+        //     }
+        // }
+
+        // return ans.toString().equals(ans.reverse().toString());
+        int start = 0;
+        int last = s.length() - 1;
+
+        while(start <= last){
+            char fc = s.charAt(start);
+            char lc = s.charAt(last);
+
+            if(!Character.isLetterOrDigit(fc)){
+                start++;
+            } else if(!Character.isLetterOrDigit(lc)){
+                last--;
+            } else {
+                if(Character.toLowerCase(fc) != Character.toLowerCase(lc)){
+                    return false;
+                }
+                start++;
+                last--;
             }
         }
-
-        return ans.toString().equals(ans.reverse().toString());
+        return true;
     }
 }
